0fe25783f260cd2bbac7805812498053684e9c6c
Fernando <alvaro1221x@gmail.com>
2019-11-15 13:58:54 -0500

    PRIMER COMMIT

---
+++ server/config/config.js
@@ -0,0 +1,39 @@
+// ============================
+//  Puerto
+// ============================
+process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Vencimiento del Token
+// ============================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+// ============================
+//  SEED de autenticación
+// ============================
+process.env.SEED = process.env.SEED || 'secret';
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+//if (process.env.NODE_ENV === 'dev') {
+//  urlDB = 'mongodb://localhost:27017/cafe';
+//} else {
+// urlDB = process.env.MONGO_URI;
+urlDB = 'mongodb+srv://user:4ozvRkeatvDCUfPf@cluster0-tzeao.mongodb.net/cafe?retryWrites=true&w=majority';
+//}
+process.env.URLDB = urlDB;
