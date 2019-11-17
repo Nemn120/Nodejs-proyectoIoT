62cd542651162c16c79609ff503b524771399aa3
Fernando <alvaro1221x@gmail.com>
2019-11-17 17:31:53 -0500

    V Entorno MDB

--- server/config/config.js
+++ server/config/config.js
@@ -2,14 +2,10 @@
 //  Puerto
 // ============================
 process.env.PORT = process.env.PORT || 3000;
-
-
 // ============================
 //  Entorno
 // ============================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
-
-
 // ============================
 //  Vencimiento del Token
 // ============================
@@ -18,23 +14,20 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // 24 horas
 // 30 días
 process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
-
-
 // ============================
 //  SEED de autenticación
 // ============================
 process.env.SEED = process.env.SEED || 'secret';
 process.env.TOKEN;
-
 // ============================
 //  Base de datos
 // ============================
 let urlDB;
-
 //if (process.env.NODE_ENV === 'dev') {
-urlDB = 'mongodb://localhost:27017/cafe';
+//urlDB = 'mongodb://localhost:27017/cafe';
 //} else {
 // urlDB = process.env.MONGO_URI;
 //urlDB = 'mongodb+srv://user:4ozvRkeatvDCUfPf@cluster0-tzeao.mongodb.net/cafe?retryWrites=true&w=majority';
+urlDB = process.env.MONGO_URI;
 //}
 process.env.URLDB = urlDB;
