df58d6d01a59197fa34febaa22187a0b1fb6b250
Fernando <alvaro1221x@gmail.com>
2019-11-18 17:42:21 -0500

    Sub CRUDs

--- server/config/config.js
+++ server/config/config.js
@@ -23,12 +23,14 @@ process.env.TOKEN;
 //  Base de datos
 // ============================
 let urlDB;
-//if (process.env.NODE_ENV === 'dev') {
-//  urlDB = 'mongodb://localhost:27017/cafe';
-//} else {
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
 
-urlDB = 'mongodb+srv://user:4ozvRkeatvDCUfPf@cluster0-tzeao.mongodb.net/cafe?retryWrites=true&w=majority';
-//urlDB = process.env.MONGO_URI;
-//}
+} else {
+    urlDB = process.env.MONGO_URI;
+
+    //urlDB = 'mongodb+srv://user:4ozvRkeatvDCUfPf@cluster0-tzeao.mongodb.net/cafe?retryWrites=true&w=majority';
+
+}
 
 process.env.URLDB = urlDB;
